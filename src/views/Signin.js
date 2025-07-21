78de9aecd6a8194fcb8b9aeb956b5ad22adfe6e4
David da Silva Lopes <20231198060023@ifrn.local>
2025-07-21 16:27:13 -0300

    fazendo a estiização do login

--- src/views/Signin.js
+++ src/views/Signin.js
@@ -27,6 +27,8 @@ import { server } from '../globals/GlobalVars';
 
 // Bibliotecas do Navigation
 import { useNavigation } from '@react-navigation/native';
+import { prerenderToNodeStream } from "react-dom/static";
+import { height, width } from "@fortawesome/free-solid-svg-icons/fa0";
 
 export default function Signin() {
     const [email, setEmail] = useState('admin@gmail.com')
@@ -63,30 +65,54 @@ export default function Signin() {
     return (
         <ScrollView contentContainerStyle={styles_login.container_login}>
             <View style={styles_login.areaimagem}>
-                Area imagem
+                <Image
+                    source={require('../img/img1_aurora.jpg')}
+                    style={{ width: '100%', height: 400 }}
+                    resizeMode="contain"
+                />
             </View>
-            <View style={styles_login.containerbox_login}>
-                <View style={styles_login.card_login}>
-                    <Text style={styles.label}><FontAwesomeIcon icon={faUser} /> E-mail:</Text>
-                    <TextInput style={styles.input}
-                        value={email}
-                        onChangeText={(email => setEmail(email))}
-                    />
-                    <Text style={styles.label}><FontAwesomeIcon icon={faLock} /> Senha:</Text>
-                    <TextInput style={styles.input}
-                        value={password}
-                        onChangeText={password => setPassword(password)}
-                        secureTextEntry={true}
+            <View style={styles_login.container}>
+
+                <View style={styles_login.arealogo}>
+                    <Image
+                        source={require('../img/logo_aurora.png')}
+                        style={{ width: 500, height: 200 }}
                     />
                 </View>
+                <View style={styles_login.card_login}>
+                    <View style={styles_login.inputContainer}>
+                        <FontAwesomeIcon icon={faUser} style={styles_login.icon} />
+                        <TextInput
+                            style={styles_login.inputText}
+                            value={email}
+                            onChangeText={setEmail}
+                            placeholder="E-mail"
+                            placeholderTextColor="#888"
+                            autoCapitalize="none"
+                            underlineColorAndroid={'transparent'}
+                        />
+                    </View>
+                    <View style={styles_login.inputContainer}>
+                        <FontAwesomeIcon icon={faLock} style={styles_login.icon} />
+                        <TextInput
+                            style={styles_login.inputText}
+                            value={password}
+                            onChangeText={setPassword}
+                            placeholder="Senha"
+                            placeholderTextColor="#888"
+                            secureTextEntry={true}
 
-                <TouchableOpacity
-                    onPress={signin}
-                >
-                    <Text style={styles.button}>OK</Text>
-                </TouchableOpacity>
+                        />
+                    </View>
+                    <TouchableOpacity
+                        onPress={signin}
+                    >
+                        <Text style={styles_login.button}>Entrar</Text>
+                    </TouchableOpacity>
+                </View>
             </View>
-        </ScrollView>
+
+        </ScrollView >
     )
 
 }
@@ -97,14 +123,25 @@ const styles_login = ({
         alignItems: 'center',
         backgroundColor: '#fff',
     },
+    container: {
+        width: '100%',
+        height: '500px',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
     areaimagem: {
         width: '100%',
-        height: 700,
         backgroundColor: '#f0f0f0',
         justifyContent: 'center',
         alignItems: 'center',
-        borderBottomWidth: 1,
         borderColor: '#ccc'
+    },
+    arealogo: {
+        width: '100%',
+        height: 200,
+        justifyContent: 'center',
+        alignItems: 'center',
     },
     containerbox_login: {
         width: '100%',
@@ -114,20 +151,47 @@ const styles_login = ({
         alignItems: 'center',
     },
     card_login: {
-        width: '40%',
+        width: '70%',
         padding: 20,
         borderRadius: 10,
-
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
     },
-    input: {
-        flex: 1,
-        fontSize: 20,
+    inputContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        borderBottomWidth: 1,
+        borderColor: '#ccc',
+        marginBottom: 25,
+        paddingVertical: 5,
+        width: '40%',
+    },
+    icon: {
+        marginRight: 10,
+        fontSize: 22,
         color: '#222',
-        fontWeight: 'bold',
-        paddingVertical: 8,
+    },
+    inputText: {
+        flex: 1,
+        height: 40,
+        fontSize: 16,
+        color: '#222',
         backgroundColor: 'transparent',
         borderWidth: 0,
-        textAlign: 'left',
+        paddingLeft: 0,
+        outline: 'none',
     },
+    button: {
+        width: '200px',
+        height: 50,
+        backgroundColor: 'black',
+        color: '#fff',
+        textAlign: 'center',
+        lineHeight: 50,
+        borderRadius: 5,
+        fontSize: 18,
+        fontWeight: 'bold',
+    }
 });
 
